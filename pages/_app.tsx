21670c898d836c1d99a2563ace1950702839dc8e
Gabriel Kuettel <gabrielkuettel@gmail.com>
2022-12-08 16:41:14 -0700

    setup use-wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect } from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  reconnectProviders,
+  initializeProviders,
+  WalletProvider,
+} from "@txnlab/use-wallet";
+
+const walletProviders = initializeProviders();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    reconnectProviders(walletProviders);
+  }, []);
+
+  return (
+    <WalletProvider value={walletProviders}>
+      <Component {...pageProps} />{" "}
+    </WalletProvider>
+  );
 }
